1f07faea099a6445733f7d3b16c4992e80c74eab
Blockman <71958682+Blockman18@users.noreply.github.com>
2021-01-19 07:41:40 -0600

    Add files via upload

---
+++ rickroll.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/pkg/browser"
+)
+
+func main() {
+	fmt.Print("you opened the exe fag bag hahahah now im going to rick roll!")
+	time.Sleep(5 * time.Second)
+	browser.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+	time.Sleep(5 * time.Second)
+
+}
